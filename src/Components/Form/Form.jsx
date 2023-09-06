ce10a5c75dc09ba279b26131fcbceec36e9dd8ca
Alexey609 <alexxx609@mail.ru>
2023-09-06 22:35:32 +0500

    add: validation form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -11,6 +11,27 @@ import Button from 'react-bootstrap/Button';
 export const Form = ({ title, handleClick }) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const [mailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const reg = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
+    if (!reg.test(String(e.target.value).toLowerCase())) {
+      setEmailError('Inccorect address');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const passwordHandler = (e) => {
+    setPass(e.target.value);
+    if (e.target.value.length < 8 || e.target.value.length > 15) {
+      setPasswordError('Inccorect password');
+    } else {
+      setPasswordError('');
+    }
+  };
 
   const submitData = () => {
     handleClick(email, pass);
@@ -26,18 +47,20 @@ export const Form = ({ title, handleClick }) => {
                 placeholder="Email"
                 aria-label="Recipient's username"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => emailHandler(e)}
               />
             </InputGroup>
+            <div className="error">{mailError}</div>
             <InputGroup className="mt-3">
               <Forms.Control
                 placeholder="Password"
                 type="password"
                 aria-label="Recipient's username"
                 value={pass}
-                onChange={(e) => setPass(e.target.value)}
+                onChange={(e) => passwordHandler(e)}
               />
             </InputGroup>
+            <div className="error">{passwordError}</div>
             <div className="button__container">
               <Button variant="outline-success" onClick={submitData}>
                 {title}
